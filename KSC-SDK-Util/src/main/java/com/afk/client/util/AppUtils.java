4ce9c33b13b8f3d3b540a073341bd8d689e1142d
ALAMUSI <alamusi@kingsoft.com>
2016-10-24 15:05:31 +0800

    升级Gradle 修改APPUtil工具类，删除无效代码

--- KSC-SDK-Util/src/main/java/com/afk/client/util/AppUtils.java
+++ KSC-SDK-Util/src/main/java/com/afk/client/util/AppUtils.java
@@ -1,12 +1,8 @@
 package com.afk.client.util;
 
 import android.content.Context;
-import android.content.pm.ActivityInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.PermissionInfo;
-import android.content.pm.ProviderInfo;
-import android.content.pm.ServiceInfo;
 
 /**
  * Created by Alamusi on 2016/8/4.
@@ -76,115 +72,5 @@ public class AppUtils {
             Logger.e("can not found package " + context.getPackageName(), e);
         }
         return null;
-    }
-
-    /**
-     * 判断Manifest文件里面是否存在Activity声明
-     *
-     * @param context
-     * @param activityName
-     * @return
-     */
-    public static boolean checkActivityExist(Context context, String activityName) {
-        try {
-            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-            for (int i = 0; i < packageInfo.activities.length; i++) {
-                ActivityInfo info = packageInfo.activities[i];
-                if (info.name.equals(activityName)) {
-                    return true;
-                }
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            Logger.e("can not found package " + context.getPackageName(), e);
-        }
-        return false;
-    }
-
-    /**
-     * 判断Manifest文件里面是否存在Service声明
-     *
-     * @param context
-     * @param serviceName
-     * @return
-     */
-    public static boolean checkServiceExist(Context context, String serviceName) {
-        try {
-            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-            for (int i = 0; i < packageInfo.services.length; i++) {
-                ServiceInfo info = packageInfo.services[i];
-                if (info.name.equals(serviceName)) {
-                    return true;
-                }
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            Logger.e("can not found package " + context.getPackageName(), e);
-        }
-        return false;
-    }
-
-    /**
-     * 判断Manifest文件里面是否存在Receiver声明
-     *
-     * @param context
-     * @param receiversName
-     * @return
-     */
-    public static boolean checkReceiversExist(Context context, String receiversName) {
-        try {
-            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-            for (int i = 0; i < packageInfo.receivers.length; i++) {
-                ActivityInfo info = packageInfo.receivers[i];
-                if (info.name.equals(receiversName)) {
-                    return true;
-                }
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            Logger.e("can not found package " + context.getPackageName(), e);
-        }
-        return false;
-    }
-
-    /**
-     * 判断Manifest文件里面是否存在Provider声明
-     *
-     * @param context
-     * @param providersName
-     * @return
-     */
-    public static boolean checkProvidersExist(Context context, String providersName) {
-        try {
-            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-            for (int i = 0; i < packageInfo.providers.length; i++) {
-                ProviderInfo info = packageInfo.providers[i];
-                if (info.name.equals(providersName)) {
-                    return true;
-                }
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            Logger.e("can not found package " + context.getPackageName(), e);
-        }
-        return false;
-    }
-
-    /**
-     * 判断Manifest文件里面是否存在Permission声明
-     *
-     * @param context
-     * @param permissionName
-     * @return
-     */
-    public static boolean checkPermissionExist(Context context, String permissionName) {
-        try {
-            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-            for (int i = 0; i < packageInfo.permissions.length; i++) {
-                PermissionInfo info = packageInfo.permissions[i];
-                if (info.name.equals(permissionName)) {
-                    return true;
-                }
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            Logger.e("can not found package " + context.getPackageName(), e);
-        }
-        return false;
     }
 }
--- KSC-Util-Demo/src/main/java/com/ksc/demo/util/MainActivity.java
+++ KSC-Util-Demo/src/main/java/com/ksc/demo/util/MainActivity.java
@@ -1,13 +1,27 @@
 package com.ksc.demo.util;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.ksc.demo.util.activity.AppUtilActivity;
+
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.id_btn_app_util:
+                Intent intent = new Intent(this, AppUtilActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
