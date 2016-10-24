cd10b2a2187d6ce2c11eb0f42793f406ca4392aa
ALAMUSI <alamusi@kingsoft.com>
2016-10-24 18:18:40 +0800

    提交权限管理Library

---
+++ PermissionManagerLibrary/src/main/java/com/afk/permission/PermissionManager.java
@@ -0,0 +1,67 @@
+package com.afk.permission;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Utility to request and check System permissions for apps targeting Android M
+ * Created by Alamusi on 2016/10/24.
+ */
+
+public class PermissionManager {
+
+    static final String KEY_PERMISSION = "permissions";
+    private static PermissionManager mInstance = null;
+    private List<String> mUnRequestedPermissions;
+    private PermissionCallBack mCallBack;
+
+    public static PermissionManager getInstance() {
+        if (mInstance == null) {
+            synchronized (PermissionManager.class) {
+                if (mInstance == null) {
+                    mInstance = new PermissionManager();
+                }
+            }
+        }
+        return mInstance;
+    }
+
+    public boolean hasPermissions(Context context, PermissionCallBack callBack, String... permissions) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+        mUnRequestedPermissions = new ArrayList<>();
+        for (String permission : permissions) {
+            if (context.checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                mUnRequestedPermissions.add(permission);
+            } else {
+                callBack.onPermissionRequestResult(new Permission(permission, true));
+            }
+        }
+        return mUnRequestedPermissions.isEmpty();
+    }
+
+    public void requestPermission(Context context, PermissionCallBack callBack, String... permissions) {
+        mCallBack = callBack;
+        if (hasPermissions(context, callBack, permissions)) {
+            return;
+        }
+        Intent intent = new Intent(context, RequestPermissionActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(KEY_PERMISSION, mUnRequestedPermissions.toArray(new String[mUnRequestedPermissions.size()]));
+        context.startActivity(intent);
+    }
+
+    void onRequestPermissionResult(String[] permissions, int[] grantResults, boolean[] shouldShowRequestPermissionRationale) {
+        for (int i = 0; i < permissions.length; i++) {
+            mUnRequestedPermissions.remove(permissions[i]);
+            boolean granted = grantResults[i] == PackageManager.PERMISSION_GRANTED;
+            mCallBack.onPermissionRequestResult(new Permission(permissions[i], granted, shouldShowRequestPermissionRationale[i]));
+        }
+    }
+}
---
+++ PermissionManagerLibrary/src/main/java/com/afk/permission/RequestPermissionActivity.java
@@ -0,0 +1,61 @@
+package com.afk.permission;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Process;
+import android.support.annotation.NonNull;
+
+@TargetApi(Build.VERSION_CODES.M)
+public class RequestPermissionActivity extends Activity {
+
+    private static final String KEY_ORIGINAL_PID = "key_original_pid";
+    private int mOriginalProcessId;
+    private int mRequestCode = 101;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState == null) {
+            mOriginalProcessId = Process.myPid();
+        } else {
+            mOriginalProcessId = savedInstanceState.getInt(KEY_ORIGINAL_PID, mOriginalProcessId);
+            if (mOriginalProcessId != Process.myPid()) {
+                finish();
+            }
+        }
+        if (savedInstanceState == null) {
+            handleIntent(getIntent());
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_ORIGINAL_PID, mOriginalProcessId);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        String[] permissions = intent.getStringArrayExtra(PermissionManager.KEY_PERMISSION);
+        requestPermissions(permissions, mRequestCode);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == mRequestCode) {
+            boolean[] mShouldShowRequestPermissionRationale = new boolean[permissions.length];
+            for (int i = 0; i < permissions.length; i++) {
+                mShouldShowRequestPermissionRationale[i] = shouldShowRequestPermissionRationale(permissions[i]);
+            }
+            PermissionManager.getInstance().onRequestPermissionResult(permissions, grantResults, mShouldShowRequestPermissionRationale);
+        }
+        finish();
+    }
+}
